452e06c3e7392bbb0b6ecf3ed7a3cba648e5b069
wongkevj <wongkevj@gmail.com>
2016-02-02 23:06:39 -0800

    FoodItem Prototype(Untested)
    Made constructor, added quantity/expiration date fields as well as
    setters/getters for each. Also added volume functionality, but commented
    it out for now to keep things simple.

--- app/src/main/java/edu/fe/util/FoodItem.java
+++ app/src/main/java/edu/fe/util/FoodItem.java
@@ -1,18 +1,32 @@
 package edu.fe.util;
 
 import android.graphics.Bitmap;
+import java.util.Calendar;
 
 @Deprecated
 public class FoodItem {
 
-    public boolean food;
-    public boolean generic;
-    public boolean card;
-
+    private boolean food;
+    private boolean generic;
+    private boolean card;
+  //  private boolean hasVolume = false;
+   // private double volume;
+    private int quantity;
+    private Calendar expDate;
     private Bitmap mImage;
     private CharSequence mH1;
     private CharSequence mH2;
     private CharSequence mH3;
+
+    public FoodItem(CharSequence h1, CharSequence h2, CharSequence h3, Bitmap image,
+                    Calendar exp, int quant) {
+        mH1 = h1;
+        mH2 = h2;
+        mH3 = h3;
+        mImage = image;
+        quantity = quant;
+        expDate = exp;
+    }
 
     public Bitmap getImage() {
         return mImage;
@@ -45,6 +59,67 @@ public class FoodItem {
     public void setHeader3(CharSequence text) {
         this.mH3 = text;
     }
+
+    //Manage quantity of item
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int q) {
+        quantity = q;
+    }
+
+    public void decQuantity() {
+        if(quantity > 0) { //can't have negative quantities
+            quantity--;
+        }
+    }
+
+    public void incQuantity() {
+        quantity++;
+    }
+
+    //returns the expiration date in a Calendar object
+    public Calendar getExpDate() {
+        return expDate;
+    }
+
+    //returns a comparison of the expiration date with the current date
+    public int timeUntilExp() {
+        Calendar now = Calendar.getInstance();
+        return expDate.compareTo(now);
+    }
+
+    //manage volume of item
+   /* public double getVolume() {
+        if(hasVolume) { //always make sure it has volume
+            return volume;
+        }
+        else {
+            return -1.0; //invalid
+        }
+    }
+
+    public void setVolume(double vol) {
+        if(hasVolume) {
+            volume = vol;
+        }
+    }
+
+
+    public void decVolume() {
+        if(hasVolume) {
+            if(volume == 0) {
+                if(quantity > 0) { //decrement quantity and reset volume
+                    decQuantity();
+                    volume = 1.0;
+                }
+            }
+            volume -= 0.25; //decrement by quarters
+        }
+    } */
+
+
 
     public boolean isFood() {
         return food;

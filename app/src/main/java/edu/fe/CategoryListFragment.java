340f644f417e8f40a258a79fb7209e1ff96dc053
ChaoticTempest <Luckyadvancement@gmail.com>
2016-02-14 00:24:26 -0800

    added swipe refresh

--- app/src/main/java/edu/fe/CategoryListFragment.java
+++ app/src/main/java/edu/fe/CategoryListFragment.java
@@ -3,8 +3,10 @@ package edu.fe;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,44 +22,65 @@ import edu.fe.backend.Category;
  */
 public class CategoryListFragment extends Fragment {
 
+    public static final int COLUMN_COUNT = 2;
     private CategoryRecyclerAdapter mAdapter;
+
+
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     public CategoryListFragment() {
 
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView
+            (LayoutInflater inflater,
+             ViewGroup container,
+             Bundle savedInstanceState)
+    {
         View view = inflater.inflate(R.layout.fragment_category_list, container, false);
 
-        if(view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
+        mSwipeRefreshLayout =
+                (SwipeRefreshLayout) view.findViewById(R.id.category_swipe_refresh_layout);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.d("DEBUG", "Refreshing categories");
+                // TODO data has changed
+                mAdapter.notifyDataSetChanged();
+                // TODO onFinishDataChanged, call
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
 
-            LinearLayoutManager lm = new LinearLayoutManager(context);
-            lm.setOrientation(LinearLayoutManager.VERTICAL);
-            recyclerView.setLayoutManager(lm);
-            recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(context, new RecyclerItemClickListener.OnItemClickListener() {
-                @Override
-                public void onItemClick(View view, int position) {
-                    // Push new Activity Here
-                    Category category = (Category) mAdapter.getItem(position);
+        Context context = view.getContext();
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.category_list);
 
-                }
-            }));
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(context, COLUMN_COUNT);
+        gridLayoutManager.setOrientation(GridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(context,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        // Push new Activity Here
+                        Category category = mAdapter.getItem(position);
 
-            mAdapter = new CategoryRecyclerAdapter(
-                    new ParseQueryAdapter.QueryFactory<Category>() {
-                        @Override
-                        public ParseQuery<Category> create() {
-                            ParseQuery<Category> query = new ParseQuery<Category>(Category.class);
-                            query.orderByAscending(Category.NAME);
+                    }
+                }));
 
-                            return query;
-                        }
-                    }, false, this.getActivity());
-            recyclerView.setAdapter(mAdapter);
-        }
+        mAdapter = new CategoryRecyclerAdapter(
+                new ParseQueryAdapter.QueryFactory<Category>() {
+                    @Override
+                    public ParseQuery<Category> create() {
+                        ParseQuery<Category> query = new ParseQuery<>(Category.class);
+                        query.orderByAscending(Category.NAME);
+
+                        return query;
+                    }
+                }, false, this.getActivity());
+        recyclerView.setAdapter(mAdapter);
+
         return view;
     }
 }

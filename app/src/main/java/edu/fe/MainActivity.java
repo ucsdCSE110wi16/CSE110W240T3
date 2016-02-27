97cc04ef4e3f575fbdba1a26d31201bb158a0057
David Zech <dzech@ucsd.edu>
2016-02-26 19:44:18 -0800

    Add Category Drawer tab and Expiring Soon Drawer Tab

--- app/src/main/java/edu/fe/MainActivity.java
+++ app/src/main/java/edu/fe/MainActivity.java
@@ -1,6 +1,5 @@
 package edu.fe;
 
-import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
@@ -10,13 +9,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.InputType;
 import android.util.Log;
 import android.view.InflateException;
 import android.view.LayoutInflater;
@@ -31,8 +28,6 @@ import android.widget.TextView;
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.afollestad.materialdialogs.Theme;
-import com.parse.ParseException;
-import com.parse.Parse;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseUser;
 import com.parse.ui.ParseLoginBuilder;
@@ -42,7 +37,6 @@ import com.vorph.utils.ExceptionHandler;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
-import java.util.concurrent.Callable;
 
 import bolts.Continuation;
 import bolts.Task;
@@ -150,8 +144,19 @@ public class MainActivity
         }
 }
 
+    private void loadExpiringSoon() {
+        FragmentManager fm = getFragmentManager();
+        fm.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        FragmentTransaction transaction = fm.beginTransaction();
+        Fragment itemFragment = new ItemListFragment.Builder()
+                                    .setQueryLimit(10) // set max date
+                                    .build();
+        transaction.replace(R.id.container, itemFragment, "expiringList").commit();
+    }
+
     private void loadCategories() {
         FragmentManager fm = getFragmentManager();
+        fm.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
         fm.beginTransaction();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         Fragment categoryFragment = new CategoryListFragment();
@@ -207,10 +212,12 @@ public class MainActivity
             }
             case R.id.nav_categories: {
                 // clear fragment stacks and replace
+                loadCategories();
                 break;
             }
             case R.id.nav_expiring: {
                 // clear fragment stacks and replace
+                loadExpiringSoon();
                 break;
             }
         }
@@ -327,7 +334,7 @@ public class MainActivity
 
     @Override
     public void onCategorySelected(Category category) {
-        ItemListFragment fragment = ItemListFragment.newInstance(category);
+        ItemListFragment fragment = new ItemListFragment.Builder().setCategory(category).build();
         getFragmentManager()
                 .beginTransaction()
                 .add(R.id.container, fragment, "item-list")

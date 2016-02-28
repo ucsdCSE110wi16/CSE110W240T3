a76c106cea2985b27bf5c2575ad016ea85b35764
Jimmy Gong <jimmyxgong@gmail.com>
2016-02-28 07:25:54 -0800

    implemented camera intent along with thumbnail, pictures are saved under the Pictures folder of the phone

--- app/src/main/java/edu/fe/entryActivity.java
+++ app/src/main/java/edu/fe/entryActivity.java
@@ -1,16 +1,29 @@
 package edu.fe;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -20,6 +33,15 @@ import lib.material.picker.date.DatePickerDialog;
 import lib.material.util.TypefaceHelper;
 
 public class entryActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int ACTION_TAKE_PHOTO_B = 1;
+    private static final String JPEG_FILE_PREFIX = "IMG_";
+    private static final String JPEG_FILE_SUFFIX = ".jpg";
+    private static final String TAG = entryActivity.class.getSimpleName();
+
+    private ImageButton mImageView;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +56,8 @@ public class entryActivity extends AppCompatActivity {
         final TextView dateField = (TextView) findViewById(R.id.expirationDateTextView);
         final Button sbtBtn = (Button) findViewById(R.id.sbtBtn);
         final Button cncBtn = (Button) findViewById(R.id.cancelBtn);
+        final Button imgBtn = (Button) findViewById(R.id.imageButton);
+        mImageView = (ImageButton) findViewById(R.id.foodImageView);
         String currentDateTimeString = DateFormat.getDateInstance().format(new Date());
         dateField.setText(currentDateTimeString);
         dateField.setTypeface(null, Typeface.BOLD);
@@ -41,6 +65,9 @@ public class entryActivity extends AppCompatActivity {
         final SpinAdapter adapter = new SpinAdapter(this, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        mImageView.getLayoutParams().height = 800;
+        mImageView.getLayoutParams().width = 600;
 
         final DatePickerDialog.OnDateSetListener onDateSetListener =
                 new DatePickerDialog.OnDateSetListener() {
@@ -80,6 +107,136 @@ public class entryActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        imgBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent(int actionCode) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            switch(actionCode) {
+                case ACTION_TAKE_PHOTO_B:
+                    File f = null;
+
+                    try {
+                        f = setUpPhotoFile();
+                        mCurrentPhotoPath = f.getAbsolutePath();
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        f = null;
+                        mCurrentPhotoPath = null;
+                    }
+                    break;
+
+                default:
+                    break;
+            } // switch
+
+            //startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent, actionCode);
+
+        }
+    }
+
+    //This is where we can use the image data captured by the camera
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //Bundle extras = data.getExtras();
+            //Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //mImageView.setImageBitmap(imageBitmap);
+            handleCameraPhoto();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    private File setUpPhotoFile() throws IOException {
+        File f = createImageFile();
+        mCurrentPhotoPath = f.getAbsolutePath();
+        return f;
+    }
+
+    private void handleCameraPhoto() {
+        if (mCurrentPhotoPath != null) {
+            setPic();
+            galleryAddPic();
+            mCurrentPhotoPath = null;
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void setPic() {
+
+		/* There isn't enough memory to open up more than a couple camera photos */
+		/* So pre-scale the target bitmap into which the file is decoded */
+
+		/* Get the size of the ImageView */
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        Log.d(TAG, "SIZE OF TARGET W" + targetW);
+        Log.d(TAG, "SIZE OF TARGET H" + targetH);
+
+		/* Get the size of the image */
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        Log.d(TAG, "PHOTOPATH" + mCurrentPhotoPath);
+
+        Log.d(TAG, "SIZE OF PHOTO W" + photoW);
+        Log.d(TAG, "SIZE OF PHOTO H" + photoH);
+
+		/* Figure out which way needs to be reduced less */
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        }
+
+        Log.d(TAG, "VALUE OF SCALEFACTOR " + scaleFactor);
+
+		/* Set bitmap options to scale the image decode target */
+        bmOptions.inJustDecodeBounds = false;
+        //bmOptions.inSampleSize = scaleFactor;
+
+		/* Decode the JPEG file into a Bitmap */
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+		/* Associate the Bitmap to the ImageView */
+        mImageView.setImageBitmap(bitmap);
+        mImageView.setVisibility(View.VISIBLE);
     }
 
 }
